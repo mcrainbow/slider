b56b6c9691ef2f2f531840d1c46008cb7bdb252c
Stephan Frolov <stefro97@yandex.ru>
2023-07-19 21:59:56 +0300

    Second commit

--- slider.js
+++ slider.js
@@ -4,19 +4,38 @@ const arrowLeft = document.querySelector(".left");
 const arrowRight = document.querySelector(".right");
 const slider = document.querySelector(".slider");
 const items = document.querySelectorAll(".item");
+const bottom = document.querySelector(".bottom");
 
+// ! Variables
 let slideNumber = 1;
+const length = items.length;
 
-arrowLeft.addEventListener("click", () => {});
+const nextSlide = () => {
+  slider.style.transform = `translateX(-${slideNumber * 598}px)`;
+  slideNumber++;
+};
+
+const getFirstSlide = () => {
+  slideNumber = 1;
+  slider.style.transform = `translateX(${0}px)`;
+};
+
+const getLastSlide = () => {
+  slider.style.transform = `translateX(-${(length - 1) * 598}px)`;
+  slideNumber = length;
+};
+
+const prevSlide = () => {
+  slider.style.transform = `translateX(-${(slideNumber - 2) * 598}px)`;
+  slideNumber--;
+};
+
+// ! Events listeners
+
+arrowLeft.addEventListener("click", () => {
+  slideNumber === 1 ? getLastSlide() : prevSlide();
+});
 
 arrowRight.addEventListener("click", () => {
-  if (slideNumber < items.length) {
-    slider.style.transform = `translateX(-${slideNumber * 500}px)`;
-  } else {
-    slideNumber = 1;
-    console.log(slideNumber);
-    slider.style.transform = `translateX(${0}px)`;
-    return 0;
-  }
-  slideNumber++;
+  slideNumber < items.length ? nextSlide() : getFirstSlide();
 });
