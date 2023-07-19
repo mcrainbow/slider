a1981ee85acfb1bfff85d78371b384e9dc854e97
Stephan Frolov <stefro97@yandex.ru>
2023-07-19 22:41:45 +0300

    Final commit

--- slider.js
+++ slider.js
@@ -15,6 +15,34 @@ const nextSlide = () => {
   slideNumber++;
 };
 
+// ! Some Work With Buttons
+
+for (let i = 0; i < length; i++) {
+  const div = document.createElement("div");
+  div.className = "button";
+  bottom.appendChild(div);
+}
+
+const buttons = document.querySelectorAll(".button");
+buttons[0].style.backgroundColor = "white";
+
+const resetBg = () => {
+  buttons.forEach((button) => {
+    button.style.removeProperty("background-color");
+  });
+};
+
+buttons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    resetBg();
+    slideNumber = index + 1;
+    slider.style.transform = `translateX(-${index * 598}px)`;
+    button.style.backgroundColor = "white";
+  });
+});
+
+// ! Slide change functions
+
 const getFirstSlide = () => {
   slideNumber = 1;
   slider.style.transform = `translateX(${0}px)`;
@@ -30,12 +58,19 @@ const prevSlide = () => {
   slideNumber--;
 };
 
+const changeColor = () => {
+  resetBg();
+  buttons[slideNumber - 1].style.backgroundColor = "white";
+};
+
 // ! Events listeners
 
 arrowLeft.addEventListener("click", () => {
   slideNumber === 1 ? getLastSlide() : prevSlide();
+  changeColor();
 });
 
 arrowRight.addEventListener("click", () => {
   slideNumber < items.length ? nextSlide() : getFirstSlide();
+  changeColor();
 });
